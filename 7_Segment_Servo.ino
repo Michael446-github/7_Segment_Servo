529d30f2a09f8ce9cf459e2499b466478347bda2
Muhammadrasul <64916997+Muhammadrasul446@users.noreply.github.com>
2020-05-31 20:04:39 +0500

    Update 7_Segment_Servo.ino

--- 7_Segment_Servo.ino
+++ 7_Segment_Servo.ino
@@ -11,10 +11,11 @@ uint8_t servo_pins[] = {2, 3, 4, 5, 6, 7, 8};
 Servo motors[7];
 
 // Here is a "Hash table" data structure
+// Please follow the order, for each symbol to be displayed correctly. So hash_values[n] must be a number for hash_keys[n]
 char hash_keys[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', ' ', '=', '_'};
 uint8_t hash_values[] = {119, 124, 57, 94, 121, 113, 116, 6, 30, 118, 56, 21, 55, 92, 115, 103, 80, 109, 120, 28, 62, 42, 118, 110, 91, 61, 6, 91, 79, 102, 109, 125, 7, 127, 111, 64, 0, 65, 8};
 
-uint16_t n = sizeof(hash_keys) / sizeof(hash_keys[0]);
+uint16_t length_of_arrays = sizeof(hash_keys) / sizeof(hash_keys[0]);
 
 String text = String(TEXT);
 
@@ -37,12 +38,12 @@ void loop()
 		update(text[i]);
 		delay(INTERVAL);
 
-		// If there are repeating chars
+		// If there are repeating symbols like 'Running', uncomment this:
 		// update('0');
 		// delay(300);
 	}
 
-	// If you want to use BT ot Serial port then:
+	// If you want to use BT ot Serial port, then uncomment this, and comment the above code:
 	//serial();
 }
 
